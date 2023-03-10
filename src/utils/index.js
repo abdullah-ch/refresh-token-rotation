8a98be8b7ab4e1a9844a452a49083191cb9d9cd9
“abdullah-ch-empg” <abdullah.ch@empglabs.com>
2023-03-10 21:05:40 +0500

    added comments in utils file

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,21 +6,13 @@ const {
 } = process.env;
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { validationResult } = require("express-validator");
 
 /**
- ** Executes the validation request and returns a JSON response.
- * @param  {object} body
- */
-const validationResponse = (body) => {
-  validationResult(body).throw();
-};
-
-/**
- ** Generates a new hash.
- * @param  {string} password
- * @return {string} hash
- */
+  
+  Generates a new hash using bcrypt.
+  @param {string} password - Password to be hashed.
+  @returns {string|false} - Returns the hashed password or false if there's an error.
+  */
 const generatePassword = async (password) => {
   try {
     const salt = await bcrypt.genSalt(10);
@@ -31,35 +23,33 @@ const generatePassword = async (password) => {
     return false;
   }
 };
-
 /**
- ** Trims and Converts the Input String to Lowercase.
- * @param  {string} str
- * @returns {string} str
- */
+  
+  Trims and converts the input string to lowercase.
+  @param {string} str - String to be trimmed and converted to lowercase.
+  @returns {string} - Returns the trimmed and lowercase string.
+  */
 const trimLowerCaseString = (str) => str.toLowerCase().replace(/\s+/g, "");
-
 /**
- * checks password
- * @param {string} password
- * @param {string} hash
- * @returns A promise to be either resolved with the comparison result salt or rejected with an Error
- */
-
-const checkPassword = async (password, hash) =>
-  await bcrypt.compare(password, hash);
-
+  
+  Checks if the provided password matches the provided hash using bcrypt.
+  @param {string} password - Password to be checked.
+  @param {string} hash - Hash to be compared against.
+  @returns {Promise<boolean>} - Returns a promise to be either resolved with the comparison result or rejected with an error.
+  */
+const checkPassword = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
 /**
- * generates access-token and refresh-token
- * @param {object} userInfo
- * @return {string} access-token and refresh-token
- */
-
+  
+  Generates a pair of access and refresh tokens using JSON Web Tokens.
+  @param {object} userInfo - User information to be stored in the token.
+  @returns {object} - Returns an object containing the access and refresh tokens.
+  */
 const generateTokenSet = (userInfo) => {
   const accessToken = jwt.sign(userInfo, JWT_SECRET_ACCESS_TOKEN, {
     expiresIn: JWT_SECRET_ACCESS_TOKEN_TIME,
   });
-
   const refreshToken = jwt.sign(userInfo, JWT_SECRET_REFRESH_TOKEN, {
     expiresIn: JWT_SECRET_REFRESH_TOKEN_TIME,
   });
@@ -69,11 +59,15 @@ const generateTokenSet = (userInfo) => {
     refreshToken,
   };
 };
+
 /**
- * decodes JWT Token
- * @param {string} token
- * @returns {object} decodedUserInformation
- */
+  
+  Decodes a JSON Web Token to extract the user information.
+  @param {string} token - Token to be decoded.
+  @param {string} secret - Secret used to sign the token.
+  @returns {object} - Returns an object containing the decoded user information.
+  @throws - Throws an error if there's a problem decoding the token.
+  */
 const extractUser = (token, secret) => {
   try {
     return jwt.verify(token, secret);
@@ -82,13 +76,18 @@ const extractUser = (token, secret) => {
     throw error;
   }
 };
-
+/**
+  
+  Decodes a JSON Web Token to extract the user information.
+  @param {string} token - Token to be decoded.
+  @param {string} secret - Secret used to sign the token.
+  @returns {object|null} - Returns an object containing the decoded user information, or null if the token is invalid or has expired.
+  */
 const decodeUser = (token, secret) => {
   return jwt.decode(token, secret);
 };
 module.exports = {
   generatePassword,
-  validationResponse,
   trimLowerCaseString,
   checkPassword,
   generateTokenSet,
