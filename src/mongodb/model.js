7a623a534d5706ba02489c858af592b5d33508b2
“abdullah-ch” <abdullahchaudharry@gmail.com>
2023-03-12 16:07:09 +0500

    fresh-branch

--- src/mongodb/model.js
+++
@@ -1,9 +0,0 @@
-//this file contains all the models
-const mongoose = require("mongoose");
-const userSchema = require("./schema/user");
-
-const userModel = mongoose.model("users", userSchema);
-
-module.exports = {
-  userModel,
-};
--- src/routers/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getUserInformation } = require("../controllers/user");
-const { verifyToken } = require("../middlewares/auth");
-const userRouter = express.Router();
-
-userRouter.get("/", verifyToken, getUserInformation);
-module.exports = userRouter;
